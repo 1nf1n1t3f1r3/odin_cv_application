c2ac868ed470817ad2f1c743dae33f8985dc467b
1nf1n1t3f1r3 <1nf1n1t3f1r3@gmail.com>
2026-05-04 11:44:03 +0200

    Added Education Field 	It's a bit weird being able to have 2 Edits open...

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,5 +1,53 @@
-function Education() {
-  return <h2>Education  Section</h2>;
+//src/components/GeneralInfo.jsx
+
+import { useState } from "react";
+import Field from "./Field.jsx";
+
+function GeneralInfo() {
+  const [school, setSchool] = useState("");
+  const [study, setStudy] = useState("");
+  const [date, setDate] = useState("");
+
+  const [editingField, setEditingField] = useState(null);
+
+  return (
+    <div>
+      <h2>General Info</h2>
+
+      <Field
+        label="School"
+        type="text"
+        value={school}
+        isEditing={editingField === "school"}
+        onChange={(e) => setSchool(e.target.value)}
+        onEditToggle={() =>
+          setEditingField(editingField === "school" ? null : "school")
+        }
+      />
+
+      <Field
+        label="Study"
+        type="study"
+        value={study}
+        isEditing={editingField === "study"}
+        onChange={(e) => setStudy(e.target.value)}
+        onEditToggle={() =>
+          setEditingField(editingField === "study" ? null : "study")
+        }
+      />
+
+      <Field
+        label="Date"
+        type="text"
+        value={date}
+        isEditing={editingField === "date"}
+        onChange={(e) => setDate(e.target.value)}
+        onEditToggle={() =>
+          setEditingField(editingField === "date" ? null : "date")
+        }
+      />
+    </div>
+  );
 }
 
-export default Education;
+export default GeneralInfo;
